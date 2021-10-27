56741918ccacb05bed457df559b18b422cb4663c
Jose <78415618+JoseAFerrer@users.noreply.github.com>
2021-10-27 18:24:44 +0200

    Created Cliente class and the .gitignore

---
+++ BankAppAiudo/Entities/Class.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankAppAiudo.Entities
+{
+    public class Cliente
+    {
+        public string Id { get; private set; }
+        public string Password { get; private set; }
+
+        public Cliente(string id, string password)
+        {
+            Id = id;
+            Password = password;
+        }
+
+        public double Balance { get; private set; }
+        //Historial: Lista de movimientos
+        //Deuda: lista de préstamos
+        //Número de cuenta
+
+    }
+}
