6be105948fb2b73ed396726f385bd4dd14c80ab8
Jose <78415618+JoseAFerrer@users.noreply.github.com>
2021-10-29 12:29:36 +0200

    Create (and only create) simplified classes to interact with the DB. They remain to be adequately written. Also create the BankAppcontext, which tells the DB what exactly is going to go inside it.

---
+++ BankAppAiudo/DbContexts/BankAppContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankAppAiudo.DbContexts
+{
+    public class BankAppContext
+    {
+    }
+}
